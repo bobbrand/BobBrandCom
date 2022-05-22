a2e07aa03de7422490f9a96fa0faa400dacaa378
Bob Brand <bobbrand@bobbrand.com>
2022-05-21 21:42:47 -0700

    Modals and validations working

--- js/contact_modal.js
+++ js/contact_modal.js
@@ -2,22 +2,21 @@
 // Bob Brand 
 // All Rights Reserved.
 
-// Click handler for Contact button.
-//    Aggregates the contact info and message and emails 
-function logSubmit(event) {
-    console.log(event.timeStamp);
-    event.preventDefault();
 
-    var validator = $("form[id='emailButtonForm']").validate({
-        rules: {
-            contactEmail: {
-                required: true,
-                email: true
-            }
-        }
-    })
+$(document).ready(function () {
 
-    if (validator.valid()) {
+    var emailContactForm = document.getElementById('emailContactForm');
+    var emailMessageForm = document.getElementById('emailMessageForm');
+
+    emailContactForm.addEventListener('submit', onClickContact);
+    emailMessageForm.addEventListener('submit', onClickMessage);
+
+    // Click handler for Contact button.
+    //    Passes the email address to the contact form
+    function onClickContact(event) {
+        // console.log(event.timeStamp);
+        event.preventDefault();   // The default submit behavior blocks the modal display :(
+
         // Pre-populate the email field with the info already entered
         var contactModal = document.getElementById('contactModal');
         var emailField = contactModal.querySelector('.modal-body input[id="form-email"]');
@@ -25,20 +24,14 @@ function logSubmit(event) {
 
         $('#contactModal').modal('show');
     }
-    else {
-        alert("Please enter a valid Email address.")
-    }
-}
 
-$(document).ready(function () {
+    // Click handler for Submit button.
+    //    Aggregates the contact info and message and emails 
+    function onClickMessage(event) {
+        //console.log(event.timeStamp);
+        event.preventDefault();
 
-    var formname = document.getElementById('emailButtonForm');
-    formname.addEventListener('submit', logSubmit);
-
-    // C0F79DFE84C21854AFCA253C6C1E0BEB0874
-    // Click Handler for the Submit button in the email modal
-    $('#btnSubmit').click(function () {
-
+        // C0F79DFE84C21854AFCA253C6C1E0BEB0874
         var msgSentToast = document.getElementById('msgSentToast');
         var myToast = bootstrap.Toast.getOrCreateInstance(msgSentToast);
 
@@ -47,7 +40,7 @@ $(document).ready(function () {
         var formEmail = $("#form-email").val()
         var formMessage = $("#form-message").val()
         var body = "First: " + formFirstName + '<br>'
-            + "Last: " + formLastName +
+            + "Last: " + formLastName + '<br>'
             + "Email: " + formEmail + '<br><br>';
         body += formMessage;
 
@@ -64,5 +57,5 @@ $(document).ready(function () {
         }).then(
             myToast.show()
         );
-    });
+    }
 });
